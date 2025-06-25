0cc71ffe7fe629b991c19e770316853be855f17d
Charlotte <cnkuna920@gmail.com>
2025-06-25 12:26:51 +0200

    added while loop

--- forloop.js
+++ forloop.js
@@ -1,3 +1,4 @@
+//for loop example
 let courses = ["JavaScript", "HTML", "CSS"]
 //create index value. the i stands for index
 for(let i=0;i< courses.length; i++){
@@ -5,3 +6,16 @@ for(let i=0;i< courses.length; i++){
     console.log(courses[i])  
     //it prints vertical
 }
+
+
+//while loop condition examples
+let val = 1;
+while (val < 6) {
+    console.log(val);
+    val += 1; // +=1 or ++ is the repeating limit
+}
+
+let va = 1;
+while (1 < 10) {
+    console.log(va); //it will continue because there is no limit
+} 
